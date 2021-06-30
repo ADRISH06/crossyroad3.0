66100ce55b14da581a1315667ade4f514dd5e434
ADRISH06 <85296245+ADRISH06@users.noreply.github.com>
2021-06-30 09:14:18 +0530

    Add files via upload

--- Car.js
+++ Car.js
@@ -5,7 +5,7 @@ class Car{
          this.y = height-random([150,250,350,950,1050,1150,1750,1850,1950]);
          this.spt = createSprite(this.x,this.y);
          this.spt.shapeColor = "blue";
-         this.spt.scale=0.07;
+         this.spt.scale=0.05;
        this.spt.addAnimation("car",carAnimation1);
          if(this.y==height-250 || this.y==height-1050 || this.y==height-1850)
          {
